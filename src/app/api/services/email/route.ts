f4b31704fef8747c4eec5b86bd37ea712e827c00
mohit <mohitthakur9901@gmail.com>
2025-06-21 14:05:27 +0530

    email api route fix

--- src/app/api/services/email/route.ts
+++ src/app/api/services/email/route.ts
@@ -2,8 +2,16 @@ import { sendEmailToMembers } from "@/app/actions/services";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  console.log("send email to members");
+  try {
+    if (process.env.NODE_ENV === "production") {
+      console.log("send email to members");
+      const result = await sendEmailToMembers();
+      return NextResponse.json(result);
+    }
 
-  const result = await sendEmailToMembers();
-  return NextResponse.json(result);
+    return NextResponse.json({ message: "Skipped in non-production" });
+  } catch (error) {
+    console.error("API Error:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
