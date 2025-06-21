c44a21e863d8d30eb32988fd21483db07ac58ef0
mohit <mohitthakur9901@gmail.com>
2025-06-21 15:27:15 +0530

    fix publishable key error

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
       "res.cloudinary.com",
     ],
   },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    Resend_API_KEY: process.env.RESEND_API_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+  }
 };
 
 export default nextConfig;
